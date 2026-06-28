6fbd46c986bbdb3629ab1670394a2abfeb8c0d22
Jeeten Mandal <jeeten.cloveit@gmail.com>
2026-06-28 15:04:37 +0545

    add cors middleware and update dependencies

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDB from './config/database.js';
 import HANDLERS from './handlers/index.js';
 import errorMiddleware from './middlewares/error.js';
 import { authMiddleware } from './middlewares/auth.js';
+import cors from cors;
 
 const app = express();
 
@@ -12,6 +13,14 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use(express.json());
+
+ app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+
 app.use(authMiddleware);
 app.use("/", HANDLERS);
 app.use(errorMiddleware);
