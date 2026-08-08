15d451ef35ff41602b19ebf9ea7edb65f8d62b78
Anish chhetri <anishkhestri-77>
2026-08-08 20:14:27 +0545

    modified index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 
  app.use(cors({
-  origin: "process.env.FRONTEND_URL",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
